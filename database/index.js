cdc8dd34a7f64ac23e90e9dd1374d69b8ec8533b
Lucas Lemos <guedeslemos29@gmail.com>
2020-09-08 20:24:53 -0300

    SET SSL REJECTION FOR HEROKU DB ACCESS

--- database/index.js
+++ database/index.js
@@ -2,9 +2,9 @@ const { Client } = require('pg');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 async function connect() {
